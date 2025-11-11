02689b974f99df9d1823989b256940a11b2d5d03
elekesnorbi <elekes.norbert.karoly@szbiszeged.hu>
2025-11-11 15:25:51 +0100

    kurva

--- backend/public/js/adatok.js
+++ backend/public/js/adatok.js
@@ -1,4 +1,4 @@
-let markak = ['Audi', 'BMW', 'Chevrolet', 'Ferrari', 'Lamborghini', 'Nissan', 'Koenigsegg', 'Opel', 'Fiat', 'Mitsubishi', 'Honda', 'Porsche', 'Maseratti', 'Jaguar', 'Bugatti'];
+let markak = ['Audi', 'BMW', 'Chevrolet', 'Ferrari', 'Lamborghini', 'Nissan', 'Koenigsegg', 'Opel', 'Fiat', 'Mitsubishi', 'Honda', 'Porsche', 'Maseratti', 'Jaguar', 'Bugatti', 'Mercedes'];
 let kedvezmenyek = [0, 10, 20, 30];
 
 module.exports = { markak, kedvezmenyek };
