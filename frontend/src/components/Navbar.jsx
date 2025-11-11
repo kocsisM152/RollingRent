579860b0e49ab199fb9e8fee997a5ab611cff170
kocsisM152 <kocsis.marko@szbiszeged.hu>
2025-11-11 14:20:01 +0100

    frontendkész?

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,12 +2,12 @@ import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
-    return (
-        <div>
-            <Link to="/">Főoldal</Link>
-            <Link to="/cars">Autók</Link>
-        </div>
-    );
+    // return (
+    //     <div>
+    //         <Link to="/">Főoldal</Link>
+    //         <Link to="/cars">Autók</Link>
+    //     </div>
+    // );
 };
 
 export default Navbar;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,15 +2,19 @@ import './Home.css';
 
 const Home = () => {
   return (
-    <div className='home-background'>
-      <div className='home-kontener'>
+    <div className="home-background">
+
+      <div className="menu">
+        <button>Főoldal</button>
+        <button>Autók</button>
+      </div>
+
+      <div className="home-kontener">
         Home
       </div>
+
     </div>
-    
   );
 };
-
-
 
 export default Home;
