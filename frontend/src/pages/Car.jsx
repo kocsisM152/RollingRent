12f41212a9902989568a3d9d44d9d6ccb78cd2db
elekesnorbi <elekes.norbert.karoly@szbiszeged.hu>
2025-11-13 14:32:19 +0100

    kutyamarko

--- frontend/src/pages/Car.jsx
+++ frontend/src/pages/Car.jsx
@@ -3,9 +3,10 @@ import "./Car.css";
 const Car = ({ kocsi }) => {
   return (
     <div className="kocsi">
-      <h1>Típus: {kocsi.tipus}</h1>
       <div className="kocsi-kep" key={kocsi._id}>
+      <h1>Típus: {kocsi.tipus}</h1>
         <img src={kocsi.kepek[0]} />
+        <h1>Részletek:</h1>
       </div>
       {/* <div className="kocsi-kepek">
                 {kocsi.kepek.map((elem) => {
